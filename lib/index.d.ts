991eac2d2aae958ef4101fe3ae57d2e809fd6f96
Santosh Joseph <sjoseph@groupon.com>
2019-12-01 08:09:47 -0800

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,18 @@
+import { Card, Rank, Suit } from 'deckjs';
+export declare type CardCallback = (player: number, card: Card) => void;
+export { Card, Rank, Suit };
+export default class BlackjackCounter {
+    private deck;
+    private cb;
+    private numOfDecks;
+    private numOfPlayers;
+    private countNum;
+    constructor(cb: CardCallback, numOfDecks?: number, numOfPlayers?: number);
+    readonly count: number;
+    shuffle(): void;
+    startGame(): void;
+    endGame(): void;
+    getCard(): Card;
+    private getCount;
+}
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import { BlackjackDeck, Card, Rank, Suit } from 'deckjs';
+
+export type CardCallback = (player:number, card:Card) => void;
+export {
+  Card,
+  Rank,
+  Suit
+}
+
+export default class BlackjackCounter {
+  private deck:BlackjackDeck;
+  private cb:CardCallback;
+  private numOfDecks:number;
+  private numOfPlayers:number;
+  private countNum:number;
+
+  constructor(cb:CardCallback, numOfDecks:number = 6,  numOfPlayers:number = 1) {
+    // TODO: strategyType
+    this.numOfDecks = numOfDecks;
+    this.numOfPlayers = numOfPlayers;
+    this.cb = cb;
+    this.deck = new BlackjackDeck(numOfDecks);
+    this.countNum = 0;
+  }
+
+  get count() {
+    return this.countNum;
+  }
+
+  public shuffle(): void {
+    this.deck.shuffle();
+  }
+
+  public startGame():void {
+    this.countNum = 0;
+    const d1 = this.deck.getCard();
+    const d2 = this.deck.getCard();
+    const p1 = this.deck.getCard();
+    const p2 = this.deck.getCard();
+    this.cb(1, p1);
+    this.countNum += this.getCount(p1);
+    this.cb(0, d1);
+    this.countNum += this.getCount(d1);
+    this.cb(1, p2);
+    this.countNum += this.getCount(p2);
+    this.cb(0, d2);
+    this.countNum += this.getCount(d2);
+  }
+
+  public endGame():void {
+  }
+
+  public getCard() {
+    const c = this.deck.getCard();
+    this.countNum += this.getCount(c);
+    return c;
+  }
+
+  private getCount(card:Card) {
+    if(card.rank === Rank.Ace ||
+       card.rank === Rank.Ten ||
+       card.rank === Rank.Jack ||
+       card.rank === Rank.Queen ||
+       card.rank === Rank.King) {
+      return -1;
+    } else if (card.rank === Rank.Two ||
+               card.rank === Rank.Three ||
+               card.rank === Rank.Four ||
+               card.rank === Rank.Five ||
+               card.rank === Rank.Six) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+/*
+ UI code will call create the class with the number of decks, strategyForCounting, players#, timeout, showcount
+   then will call start() with call backs
+   callback -> player#, cards
+   getCard() will return a card (when player hits)
+   +, - for keeping count or -2, -1, 0, +1, +2 for buttons and have a highlight over each card
+   OR
+   +, - buttons and then prompt the player for action and then +/-. then dealer happens every 1 second
+   
+
+*/
